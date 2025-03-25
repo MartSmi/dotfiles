428f3b6780fe5636241a4ef6c9d42e96920a9a13
Martynas Smitas <martynas.smi@proton.me>
2025-03-25 17:54:47 +0100

    feat: add makefile

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,18 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+    export EDITOR=neovide
+
+    alias r=ranger
+    alias l=la
+    alias mk=mkdir
+    alias t='swaync-client --dnd-on && termdown -s $argv && swaync-client --dnd-off && notify-send "break"'
+    alias nst='termdown -s $argv && notify-send "break"'
+    alias yay='yay --noconfirm'
+    alias g='ping google.com'
+
+end
+
+function v
+    neovide $argv & disown
+end
--- nvim/.config/nvim/lua/theprimeagen/init.lua
+++ nvim/.config/nvim/lua/theprimeagen/init.lua
@@ -52,7 +52,7 @@ autocmd('BufEnter', {
         if vim.bo.filetype == "zig" then
             vim.cmd.colorscheme("tokyonight-night")
         else
-            vim.cmd.colorscheme("brightburn")
+            vim.cmd.colorscheme("gruvbox")
         end
     end
 })
@@ -66,7 +66,7 @@ autocmd('LspAttach', {
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
         vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
